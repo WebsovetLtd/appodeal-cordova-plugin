297e0084013576944cd3cb921188fa95082fbb35
loonybtard <MsDos23525@yandex.ru>
2025-02-02 18:32:08 +0300

    Reformatted js/ts files

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,60 +1,60 @@
 interface Window {
-    plugins: Plugins
+  plugins: Plugins
 }
 
 interface Plugins {
-    Appodeal: Appodeal
+  Appodeal: Appodeal
 }
 
 declare class Appodeal {
-    initialize: (appKey: any, adTypes: any, hasConsent: any, callback: any) => void
-    manageConsent: (appKey: any, adTypes: any, hasConsent: any, callback: any) => void
-    show: (adType: any, callback: any) => void
-    showWithPlacement: (adType: any, placement: any, callback: any) => void
-    showBannerView: (xAxis: any, yAxis: any, placement: any) => void
-    isLoaded: (adType: any, callback: any) => void
-    cache: (adType: any) => void
-    hide: (adType: any) => void
-    destroy: (adType: any) => void
-    onResume: (adType: any) => void
-    setAutoCache: (adTypes: any, autoCache: any) => void
-    isPrecache: (adType: any, callback: any) => void
-    setBannerAnimation: (value: any) => void
-    setSmartBanners: (value: any) => void
-    set728x90Banners: (value: any) => void
-    setBannerOverLap: (value: any) => void
-    refreshWebViewForBanner: (value: any) => void
-    setTesting: (testing: any) => void
-    setLogLevel: (loglevel: any) => void
-    setChildDirectedTreatment: (value: any) => void
-    setTriggerOnLoadedOnPrecache: (set: any) => void
-    disableNetwork: (network: any, adType: any) => void
-    disableNetworkType: (network: any, adType: any) => void
-    disableLocationPermissionCheck: () => void
-    disableWriteExternalStoragePermissionCheck: () => void
-    muteVideosIfCallsMuted: (value: any) => void
-    showTestScreen: (value: any) => void
-    getVersion: (callback: any) => void
-    getPluginVersion: () => string
-    isInitialized: (adTypes: any, callback: any) => void
-    canShow: (adType: any, callback: any) => void
-    canShowWithPlacement: (adType: any, placement: any, callback: any) => void
-    getRewardParameters: (callback: any) => void
-    getRewardParametersForPlacement: (placement: any, callback: any) => void
-    setExtraData: (name: any, value: any) => void
-    getPredictedEcpm: (adType: any, callback: any) => void
-    setAge: (age: any) => void
-    setGender: (gender: any) => void
-    setUserId: (userid: any) => void
-    trackInAppPurchase: (amount: any, currency: any) => void
-    hasStatusBarPlugin: (value: any) => void
-    setInterstitialCallbacks: (callback: any) => void
-    setNonSkippableVideoCallbacks: (callbacks: any) => void
-    setRewardedVideoCallbacks: (callbacks: any) => void
-    setBannerCallbacks: (callbacks: any) => void
-    setNativeCallbacks: (callback: any) => void
-    getNativeAds: (callback: any) => void
-    setNativeAdPosition: (x: any, y: any, w: any, h: any, tabH: any, callback: any) => void
-    hideNativeAd: (callback: any) => void
-    revealHiddenNativeAd: (callback: any) => void
+  initialize: (appKey: any, adTypes: any, callback: any) => void
+  manageConsent: (appKey: any, adTypes: any, hasConsent: any, callback: any) => void
+  show: (adType: any, callback: any) => void
+  showWithPlacement: (adType: any, placement: any, callback: any) => void
+  showBannerView: (xAxis: any, yAxis: any, placement: any) => void
+  isLoaded: (adType: any, callback: any) => void
+  cache: (adType: any) => void
+  hide: (adType: any) => void
+  destroy: (adType: any) => void
+  onResume: (adType: any) => void
+  setAutoCache: (adTypes: any, autoCache: any) => void
+  isPrecache: (adType: any, callback: any) => void
+  setBannerAnimation: (value: any) => void
+  setSmartBanners: (value: any) => void
+  set728x90Banners: (value: any) => void
+  setBannerOverLap: (value: any) => void
+  refreshWebViewForBanner: (value: any) => void
+  setTesting: (testing: boolean) => void
+  setLogLevel: (loglevel: any) => void
+  setChildDirectedTreatment: (value: any) => void
+  setTriggerOnLoadedOnPrecache: (set: any) => void
+  disableNetwork: (network: any, adType: any) => void
+  disableNetworkType: (network: any, adType: any) => void
+  disableLocationPermissionCheck: () => void
+  disableWriteExternalStoragePermissionCheck: () => void
+  muteVideosIfCallsMuted: (value: any) => void
+  showTestScreen: (value: any) => void
+  getVersion: (callback: any) => void
+  getPluginVersion: () => string
+  isInitialized: (adTypes: any, callback: any) => void
+  canShow: (adType: any, callback: any) => void
+  canShowWithPlacement: (adType: any, placement: any, callback: any) => void
+  getRewardParameters: (callback: any) => void
+  getRewardParametersForPlacement: (placement: any, callback: any) => void
+  setExtraData: (name: any, value: any) => void
+  getPredictedEcpm: (adType: any, callback: any) => void
+  setAge: (age: any) => void
+  setGender: (gender: any) => void
+  setUserId: (userid: any) => void
+  trackInAppPurchase: (amount: any, currency: any) => void
+  hasStatusBarPlugin: (value: any) => void
+  setInterstitialCallbacks: (callback: any) => void
+  setNonSkippableVideoCallbacks: (callbacks: any) => void
+  setRewardedVideoCallbacks: (callbacks: any) => void
+  setBannerCallbacks: (callbacks: any) => void
+  setNativeCallbacks: (callback: any) => void
+  getNativeAds: (callback: any) => void
+  setNativeAdPosition: (x: any, y: any, w: any, h: any, tabH: any, callback: any) => void
+  hideNativeAd: (callback: any) => void
+  revealHiddenNativeAd: (callback: any) => void
 }
--- www/index.js
+++ www/index.js
@@ -19,9 +19,9 @@ Appodeal.BANNER_X_LEFT = 2;
 Appodeal.BANNER_X_RIGHT = 3;
 
 Appodeal.LogLevel = {
-    NONE: 0,
-    DEBUG: 1,
-    VERBOSE: 2
+  NONE: 0,
+  DEBUG: 1,
+  VERBOSE: 2
 };
 
 Appodeal.Gender = {
@@ -41,194 +41,194 @@ Appodeal.manageConsent = function(appKey, adTypes, hasConsent, callback) {
     exec(callback, null, SERVICE, "manageConsent", [appKey, adTypes, hasConsent]);
 };
 
-Appodeal.show = function(adType, callback) {
-    exec(callback, null, SERVICE, "show", [adType]);
+Appodeal.show = function (adType, callback) {
+  exec(callback, null, SERVICE, "show", [adType]);
 };
 
-Appodeal.showWithPlacement = function(adType, placement, callback) {
-    exec(callback, null, SERVICE, "showWithPlacement", [adType, placement]);
+Appodeal.showWithPlacement = function (adType, placement, callback) {
+  exec(callback, null, SERVICE, "showWithPlacement", [adType, placement]);
 };
 
-Appodeal.showBannerView = function(xAxis, yAxis, placement) {
-    exec(null, null, SERVICE, "showBannerView", [xAxis, yAxis, placement]);
+Appodeal.showBannerView = function (xAxis, yAxis, placement) {
+  exec(null, null, SERVICE, "showBannerView", [xAxis, yAxis, placement]);
 };
 
-Appodeal.isLoaded = function(adType, callback) {
-    exec(callback, null, SERVICE, "isLoaded", [adType]);
+Appodeal.isLoaded = function (adType, callback) {
+  exec(callback, null, SERVICE, "isLoaded", [adType]);
 };
 
-Appodeal.cache = function(adType) {
-    exec(null, null, SERVICE, "cache", [adType]);
+Appodeal.cache = function (adType) {
+  exec(null, null, SERVICE, "cache", [adType]);
 };
 
-Appodeal.hide = function(adType) {
-    exec(null, null, SERVICE, "hide", [adType]);
+Appodeal.hide = function (adType) {
+  exec(null, null, SERVICE, "hide", [adType]);
 };
 
-Appodeal.destroy = function(adType) {
-	exec(null, null, SERVICE, "destroy", [adType]);
+Appodeal.destroy = function (adType) {
+  exec(null, null, SERVICE, "destroy", [adType]);
 }
 
-Appodeal.onResume = function(adType) {
-	exec(null, null, SERVICE, "onResume", [adType]);
+Appodeal.onResume = function (adType) {
+  exec(null, null, SERVICE, "onResume", [adType]);
 }
 
-Appodeal.setAutoCache = function(adTypes, autoCache) {
-    exec(null, null, SERVICE, "setAutoCache", [adTypes, autoCache]);
+Appodeal.setAutoCache = function (adTypes, autoCache) {
+  exec(null, null, SERVICE, "setAutoCache", [adTypes, autoCache]);
 };
 
-Appodeal.isPrecache = function(adType, callback) {
-    exec(callback, null, SERVICE, "isPrecache", [adType]);
+Appodeal.isPrecache = function (adType, callback) {
+  exec(callback, null, SERVICE, "isPrecache", [adType]);
 };
 
-Appodeal.setBannerAnimation = function(value) {
-    exec(null, null, SERVICE, "setBannerAnimation", [value]);
+Appodeal.setBannerAnimation = function (value) {
+  exec(null, null, SERVICE, "setBannerAnimation", [value]);
 };
 
-Appodeal.setSmartBanners = function(value) {
-    exec(null, null, SERVICE, "setSmartBanners", [value]);
+Appodeal.setSmartBanners = function (value) {
+  exec(null, null, SERVICE, "setSmartBanners", [value]);
 };
 
-Appodeal.set728x90Banners = function(value) {
-    exec(null, null, SERVICE, "set728x90Banners", [value]);
+Appodeal.set728x90Banners = function (value) {
+  exec(null, null, SERVICE, "set728x90Banners", [value]);
 };
 
-Appodeal.setBannerOverLap = function(value) {
-    exec(null, null, SERVICE, "setBannerOverLap", [value]);
+Appodeal.setBannerOverLap = function (value) {
+  exec(null, null, SERVICE, "setBannerOverLap", [value]);
 };
 
-Appodeal.refreshWebViewForBanner = function() {
-    exec(null, null, SERVICE, "refreshWebViewForBanner", []);
+Appodeal.refreshWebViewForBanner = function () {
+  exec(null, null, SERVICE, "refreshWebViewForBanner", []);
 };
 
-Appodeal.setTesting = function(testing) {
-    exec(null, null, SERVICE, "setTesting", [testing]);
+Appodeal.setTesting = function (testing) {
+  exec(null, null, SERVICE, "setTesting", [testing]);
 };
 
-Appodeal.setLogLevel = function(loglevel) {
-    exec(null, null, SERVICE, "setLogLevel", [loglevel]);
+Appodeal.setLogLevel = function (loglevel) {
+  exec(null, null, SERVICE, "setLogLevel", [loglevel]);
 };
 
-Appodeal.setChildDirectedTreatment = function(value) {
-    exec(null, null, SERVICE, "setChildDirectedTreatment", [value]);
+Appodeal.setChildDirectedTreatment = function (value) {
+  exec(null, null, SERVICE, "setChildDirectedTreatment", [value]);
 };
 
-Appodeal.setTriggerOnLoadedOnPrecache = function(set) {
-    exec(null, null, SERVICE, "setOnLoadedTriggerBoth", [set]);
+Appodeal.setTriggerOnLoadedOnPrecache = function (set) {
+  exec(null, null, SERVICE, "setOnLoadedTriggerBoth", [set]);
 };
 
-Appodeal.disableNetwork = function(network, adType) {
-    exec(null, null, SERVICE, "disableNetwork", [network]);
+Appodeal.disableNetwork = function (network, adType) {
+  exec(null, null, SERVICE, "disableNetwork", [network]);
 };
 
-Appodeal.disableNetworkType = function(network, adType) {
-    exec(null, null, SERVICE, "disableNetworkType", [network, adType]);
+Appodeal.disableNetworkType = function (network, adType) {
+  exec(null, null, SERVICE, "disableNetworkType", [network, adType]);
 };
 
-Appodeal.disableLocationPermissionCheck = function() {
-    exec(null, null, SERVICE, "disableLocationPermissionCheck", []);
+Appodeal.disableLocationPermissionCheck = function () {
+  exec(null, null, SERVICE, "disableLocationPermissionCheck", []);
 };
 
-Appodeal.disableWriteExternalStoragePermissionCheck = function() {
-    exec(null, null, SERVICE, "disableWriteExternalStoragePermissionCheck", []);
+Appodeal.disableWriteExternalStoragePermissionCheck = function () {
+  exec(null, null, SERVICE, "disableWriteExternalStoragePermissionCheck", []);
 };
 
-Appodeal.muteVideosIfCallsMuted = function(value) {
-    exec(null, null, SERVICE, "muteVideosIfCallsMuted", [value]);
+Appodeal.muteVideosIfCallsMuted = function (value) {
+  exec(null, null, SERVICE, "muteVideosIfCallsMuted", [value]);
 };
 
-Appodeal.showTestScreen = function(value) {
-    exec(null, null, SERVICE, "showTestScreen", []);
+Appodeal.showTestScreen = function () {
+  exec(null, null, SERVICE, "showTestScreen", []);
 };
 
-Appodeal.getVersion = function(callback) {
-    exec(callback, null, SERVICE, "getVersion", []);
+Appodeal.getVersion = function (callback) {
+  exec(callback, null, SERVICE, "getVersion", []);
 };
 
-Appodeal.getPluginVersion = function(){
-    return Appodeal.pluginVersion;
+Appodeal.getPluginVersion = function () {
+  return Appodeal.pluginVersion;
 };
 
-Appodeal.isInitialized = function(adTypes, callback) {
-    exec(callback, null, SERVICE, "isInitialized", [adTypes]);
+Appodeal.isInitialized = function (adTypes, callback) {
+  exec(callback, null, SERVICE, "isInitialized", [adTypes]);
 };
 
-Appodeal.canShow = function(adType, callback) {
-    exec(callback, null, SERVICE, "canShow", [adType]);
+Appodeal.canShow = function (adType, callback) {
+  exec(callback, null, SERVICE, "canShow", [adType]);
 };
 
-Appodeal.canShowWithPlacement = function(adType, placement, callback) {
-    exec(callback, null, SERVICE, "canShowWithPlacement", [adType, placement]);
+Appodeal.canShowWithPlacement = function (adType, placement, callback) {
+  exec(callback, null, SERVICE, "canShowWithPlacement", [adType, placement]);
 };
 
-Appodeal.getRewardParameters = function(callback) {
-    exec(callback, null, SERVICE, "getRewardParameters", []);
+Appodeal.getRewardParameters = function (callback) {
+  exec(callback, null, SERVICE, "getRewardParameters", []);
 };
 
-Appodeal.getRewardParametersForPlacement = function(placement, callback) {
-    exec(callback, null, SERVICE, "getRewardParametersForPlacement", [placement]);
+Appodeal.getRewardParametersForPlacement = function (placement, callback) {
+  exec(callback, null, SERVICE, "getRewardParametersForPlacement", [placement]);
 };
 
-Appodeal.setExtraData = function(name, value) {
-    exec(null, null, SERVICE, "setExtraData", [name, value]);
+Appodeal.setExtraData = function (name, value) {
+  exec(null, null, SERVICE, "setExtraData", [name, value]);
 };
 
-Appodeal.getPredictedEcpm = function(adType, callback) {
-    exec(callback, null, SERVICE, "getPredictedEcpm", [adType]);
+Appodeal.getPredictedEcpm = function (adType, callback) {
+  exec(callback, null, SERVICE, "getPredictedEcpm", [adType]);
 };
 
-Appodeal.setAge = function(age) {
-    exec(null, null, SERVICE, "setAge", [age]);
+Appodeal.setAge = function (age) {
+  exec(null, null, SERVICE, "setAge", [age]);
 };
 
-Appodeal.setGender = function(gender) {
-    exec(null, null, SERVICE, "setGender", [gender]);
+Appodeal.setGender = function (gender) {
+  exec(null, null, SERVICE, "setGender", [gender]);
 };
 
-Appodeal.setUserId = function(userid){
-    exec(null, null, SERVICE, "setUserId", [userid]);
+Appodeal.setUserId = function (userid) {
+  exec(null, null, SERVICE, "setUserId", [userid]);
 };
 
-Appodeal.trackInAppPurchase = function(amount, currency){
-    exec(null, null, SERVICE, "trackInAppPurchase", [amount, currency]);
+Appodeal.trackInAppPurchase = function (amount, currency) {
+  exec(null, null, SERVICE, "trackInAppPurchase", [amount, currency]);
 };
 
-Appodeal.hasStatusBarPlugin = function(value){
-    exec(null, null, SERVICE, "hasStatusBarPlugin", [value]);
+Appodeal.hasStatusBarPlugin = function (value) {
+  exec(null, null, SERVICE, "hasStatusBarPlugin", [value]);
 };
 
-Appodeal.setInterstitialCallbacks = function(callback) {
-    exec(callback, null, SERVICE, "setInterstitialCallbacks", [])
+Appodeal.setInterstitialCallbacks = function (callback) {
+  exec(callback, null, SERVICE, "setInterstitialCallbacks", [])
 };
 
-Appodeal.setNonSkippableVideoCallbacks = function(callback) {
-    exec(callback, null, SERVICE, "setNonSkippableVideoCallbacks", []);
+Appodeal.setNonSkippableVideoCallbacks = function (callback) {
+  exec(callback, null, SERVICE, "setNonSkippableVideoCallbacks", []);
 };
 
-Appodeal.setRewardedVideoCallbacks = function(callback) {
-    exec(callback, null, SERVICE, "setRewardedVideoCallbacks", []);
+Appodeal.setRewardedVideoCallbacks = function (callback) {
+  exec(callback, null, SERVICE, "setRewardedVideoCallbacks", []);
 };
 
-Appodeal.setBannerCallbacks = function(callback) {
-    exec(callback, null, SERVICE, "setBannerCallbacks", []);
+Appodeal.setBannerCallbacks = function (callback) {
+  exec(callback, null, SERVICE, "setBannerCallbacks", []);
 };
 
-Appodeal.setNativeCallbacks = function(callback) {
-    exec(callback, null, SERVICE, "setNativeCallbacks", [])
+Appodeal.setNativeCallbacks = function (callback) {
+  exec(callback, null, SERVICE, "setNativeCallbacks", [])
 };
 
-Appodeal.getNativeAds = function(callback) {
-    exec(callback, null, SERVICE, "getNativeAds", [])
+Appodeal.getNativeAds = function (callback) {
+  exec(callback, null, SERVICE, "getNativeAds", [])
 }
 
-Appodeal.setNativeAdPosition = function(x, y, w, h, tabH, callback) {
-    exec(callback, null, SERVICE, "setNativeAdPosition", [x, y, w, h, tabH])
+Appodeal.setNativeAdPosition = function (x, y, w, h, tabH, callback) {
+  exec(callback, null, SERVICE, "setNativeAdPosition", [x, y, w, h, tabH])
 }
 
-Appodeal.hideNativeAd = function(callback) {
-    exec(callback, null, SERVICE, "hideNativeAd", [])
+Appodeal.hideNativeAd = function (callback) {
+  exec(callback, null, SERVICE, "hideNativeAd", [])
 }
 
-Appodeal.revealHiddenNativeAd = function(callback) {
-    exec(callback, null, SERVICE, "revealHiddenNativeAd", [])
+Appodeal.revealHiddenNativeAd = function (callback) {
+  exec(callback, null, SERVICE, "revealHiddenNativeAd", [])
 }
